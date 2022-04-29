bde2e2c3a1dd5ff4bc994520a5665ad544f3adee
FJ400 <faheemandyjp@gmail.com>
2022-04-29 14:48:14 -0400

    added background

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+
+// API call
+const api = {
+  key: "44082ee5fdfb89d4da8cad9697c38e7f",
+  url: "https://api.openweathermap.org/data/2.5/weather"
+}
 
 function App() {
+
+  const date = (e) => {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Satuday", "Sunday"];
+  }
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome to the weather app</h1>
+
+      {/* Search bar to grab user location */}
+      <input
+      type="text"
+      className="search-bar"
+      placeholder="Select location..."
+      />
     </div>
   );
 }
