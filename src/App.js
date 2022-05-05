2d9767cd22e51c5008c79ba7b880e579331e3de6
FJ400 <faheemandyjp@gmail.com>
2022-05-05 01:28:01 -0400

    fixed VH issues

--- src/App.js
+++ src/App.js
@@ -13,42 +13,40 @@ const App = () => {
   return (
     <div className="App">
       <main>
-        <h1>Welcome to the weather app</h1>
-        <h2>Get the right temperature and dress according to the weather:</h2>
+        <header>
+          <h1>Welcome to the weather app</h1>
+        </header>
+        <section>
+          <h2>Get the right temperature and dress according to the weather:</h2>
 
-        {/* Search bar to grab user location */}
-        <form
-          action=""
-          onSubmit={(e) => {
-            e.preventDefault();
-            if (cityName) {
-              setShouldFetch(true);
-            }
-          }}
-        >
-          <input
-            type="text"
-            className="search-bar"
-            placeholder="Select location..."
-            value={cityName}
-            onChange={(handleSearchBarInput) => {
-              setCityName(handleSearchBarInput.target.value);
+          {/* Search bar to grab user location */}
+          <form
+            action=""
+            onSubmit={(e) => {
+              e.preventDefault();
+              if (cityName) {
+                setShouldFetch(true);
+              }
             }}
-          />
-          <div className="buttonContainer">
-            <button>Get the weather</button>
+          >
+            <input
+              type="text"
+              className="search-bar"
+              placeholder="Select location..."
+              value={cityName}
+              onChange={(handleSearchBarInput) => {
+                setCityName(handleSearchBarInput.target.value);
+              }}
+            />
+            <div className="buttonContainer">
+              <button>Get the weather</button>
+              {/* {shouldFetch ? <FetchedDataDisplay {...{ cityName }} /> : null} */}
+            </div>
             {shouldFetch ? <FetchedDataDisplay {...{ cityName }} /> : null}
-          </div>
-        </form>
+          </form>
+        </section>
       </main>
       <Footer />
-      {/* <footer className="footer">
-        <h4>
-          Created at <a href="https://junocollege.com/">Juno College</a> &
-          developed by
-          <a href="https://www.developedbyfj.com/"> FJ</a>.
-        </h4>
-      </footer> */}
     </div>
   );
 };
